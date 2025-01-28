a382e6d28387d951531c579866bcd78a6a3bdf5a
Sergey Groshev <wer113wer@yandex.ru>
2025-01-29 00:54:35 +0300

    [feat](app): add routes for pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,18 @@
 import '../App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {AllCommentsPage} from "../pages/allComments";
+import {DetailedCommentPage} from "../pages/detailedComment";
 
 function App() {
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<AllCommentsPage/>}/>
+                <Route path="/detailedComment" element={<DetailedCommentPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
