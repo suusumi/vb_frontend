358bb41f292cb7ddb5b3bffe2ecd112aa39f6fc7
Sergey Groshev <wer113wer@yandex.ru>
2025-01-30 13:43:14 +0300

    [feat](app): update route path for detailedCommentPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,22 @@
-import '../App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {AllCommentsPage} from "../pages/allComments";
 import {DetailedCommentPage} from "../pages/detailedComment";
 import {Header} from "../widgets/header";
+import {Container} from "@mui/material";
 
 function App() {
 
     return (
         <BrowserRouter>
             <Header/>
-            <Routes>
-                <Route path="/" element={<AllCommentsPage/>}/>
-                <Route path="/detailedComment" element={<DetailedCommentPage/>}/>
-            </Routes>
+            <Container maxWidth="lg">
+                <Routes>
+                    <Route path="/" element={<AllCommentsPage/>}/>
+                    <Route path="/comment/:id" element={<DetailedCommentPage />} />
+                </Routes>
+            </Container>
         </BrowserRouter>
-    )
+    );
 }
 
 export default App
