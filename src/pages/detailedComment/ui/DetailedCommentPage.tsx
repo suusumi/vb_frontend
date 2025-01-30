c02a0bdb6a9a907bb547f05463b4f750f574a5e5
Sergey Groshev <wer113wer@yandex.ru>
2025-01-30 13:42:32 +0300

    [feat](DetailedCommentPage): write all comments page

--- src/pages/detailedComment/ui/DetailedCommentPage.tsx
+++ src/pages/detailedComment/ui/DetailedCommentPage.tsx
@@ -1,7 +1,86 @@
 import * as React from "react";
+import {useParams, useNavigate} from "react-router-dom";
+import {useGetCommentByIdQuery} from "../../../entities/comments/model/commentsApi.ts";
+import {
+    Container,
+    CircularProgress,
+    Alert,
+    Typography,
+    Stack,
+    Box,
+    Button
+} from "@mui/material";
 
 export const DetailedCommentPage: React.FC = () => {
+    const {id} = useParams();
+    const navigate = useNavigate();
+
+    const {data, isLoading, error} = useGetCommentByIdQuery(id!);
+
+    if (isLoading) {
+        return (
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "30vh",
+                }}
+            >
+                <CircularProgress/>
+            </Box>
+        );
+    }
+
+    if (error) {
+        return (
+            <Container sx={{mt: 4}}>
+                <Alert severity="error">Ошибка при загрузке комментария</Alert>
+            </Container>
+        );
+    }
+
+    if (!data) {
+        return (
+            <Container sx={{mt: 4}}>
+                <Alert severity="warning">Комментарий не найден</Alert>
+            </Container>
+        );
+    }
+
+    // Обработка перехода
+    const goToAllCommentsByPost = () => {
+        navigate(`/?postId=${data.postId}&page=1&limit=10`);
+    };
+
     return (
-        <><h1>Detailed Comments</h1></>
+        <Container maxWidth="md" sx={{mt: 4}}>
+            <Typography variant="h4" gutterBottom>
+                Комментарий
+            </Typography>
+
+            <Stack spacing={2}>
+                <Typography>
+                    <strong>ID:</strong> {data.id}
+                </Typography>
+                <Typography>
+                    <strong>Post ID:</strong> {data.postId}
+                </Typography>
+                <Typography>
+                    <strong>Name:</strong> {data.name}
+                </Typography>
+                <Typography>
+                    <strong>Body:</strong> {data.body}
+                </Typography>
+
+                <Button
+                    variant="contained"
+                    onClick={goToAllCommentsByPost}
+                    sx={{alignSelf: "flex-start"}}
+                >
+                    Посмотреть все комментарии к посту
+                </Button>
+            </Stack>
+        </Container>
     );
 };
